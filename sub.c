184770594c07cd2231af5639cd9960f0a4bb6721
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-09-25 18:04:10 +0300

    sub.c

---
+++ sub.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+* add - Adds the top two elements of the stack.
+* @stack: Double pointer to the stack.
+* @line_number: The line number in the bytecode file.
+*/
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first;
+	stack_t *second;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	first = *stack;
+	second = first->next;
+
+	second->n -= first->n; /* Add top two elements */
+	pop(stack, line_number); /* Pop the top element */
+}
