8cb02ee3b250d08e5f372f788a24529c4203926e
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-09-25 17:10:15 +0300

    add.c

--- add.c
+++ add.c
@@ -6,14 +6,17 @@
 */
 void add(stack_t **stack, unsigned int line_number)
 {
+	stack_t *first;
+	stack_t *second;
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	stack_t *first = *stack;
-	stack_t *second = first->next;
+	first = *stack;
+	second = first->next;
 
 	second->n += first->n; /* Add top two elements */
 	pop(stack, line_number); /* Pop the top element */
