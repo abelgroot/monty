12536d65ba7982afd86986f2e7209b891a40d262
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-09-25 14:52:43 +0300

    push

--- instructions.c
+++ instructions.c
@@ -9,32 +9,24 @@
  * Description: The function pushes a value onto the stack.
  * If no valid integer is given, the function exits with an error.
  */
-void push(stack_t **stack, unsigned int line_number, char *arg)
+void push(stack_t **stack, unsigned int line_number)
 {
-	int n;
-	stack_t *new_node;
+	char *arg = strtok(NULL, " \t\n");
+  if (arg == NULL || !isdigit(arg[0]) && arg[0] != '-')
+  {
+    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+    exit(EXIT_FAILURE);
+  }
 
-	if (!arg || isdigit(arg[0]) == 0)
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	n = atoi(arg);
-	new_node = malloc(sizeof(stack_t));
-	if (!new_node)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = n;
-	new_node->next = *stack;
-	new_node->prev = NULL;
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
+  stack_t *new_node = safe_malloc(sizeof(stack_t));
+  new_node->n = atoi(arg);
+  new_node->next = *stack;
+  new_node->prev = NULL;
+  if (*stack)
+  	(*stack)->prev = new_node;
+  *stack = new_node;
 }
+
 
 /**
  * pall - prints all values on the stack, starting from the top.
